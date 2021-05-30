44a588461a7583d2c910b555dacf2eeef7d67ac8
acaranta <arthur@caranta.com>
2021-05-30 10:17:40 +0000

    Added new API modules and first implementation try

--- app/getstats.py
+++ app/getstats.py
@@ -12,7 +12,14 @@ from pprint import pprint
 from datetime import datetime
 from pprint import pprint
 
-from WebAPI import WeConnect
+import logging
+from vw_utilities import read_config, json_loads
+from vw_vehicle import Vehicle
+import vw_connection
+
+from aiohttp import ClientSession
+
+logging.basicConfig(level=logging.DEBUG)
 
 #### OPTS ####
 waitTimeOnError=240
@@ -32,7 +39,95 @@ if os.getenv('COUNTRY_LANG') != None:
       COUNTRY_LANG = os.getenv('COUNTRY_LANG')
 
 
-vwc = WeConnect(COUNTRY_LANG)
+
+
+COMPONENTS = {
+    'sensor': 'sensor',
+    'binary_sensor': 'binary_sensor',
+    'lock': 'lock',
+    'device_tracker': 'device_tracker',
+    'switch': 'switch',
+    'climate': 'climate'
+}
+
+RESOURCES = [
+    'position',
+    'distance',
+    'electric_climatisation',
+    'combustion_climatisation',
+    'window_heater',
+    'combustion_engine_heating',
+    'charging',
+    'adblue_level',
+    'battery_level',
+    'fuel_level',
+    'service_inspection',
+    'oil_inspection',
+    'last_connected',
+    'charging_time_left',
+    'electric_range',
+    'combustion_range',
+    'combined_range',
+    'charge_max_ampere',
+    'climatisation_target_temperature',
+    'external_power',
+    'parking_light',
+    'climatisation_without_external_power',
+    'door_locked',
+    'trunk_locked',
+    'request_in_progress',
+    'windows_closed',
+    'sunroof_closed',
+    'trip_last_average_speed',
+    'trip_last_average_electric_consumption',
+    'trip_last_average_auxillary_consumption',
+    'trip_last_average_fuel_consumption',
+    'trip_last_duration',
+    'trip_last_length'
+]
+
+def is_enabled(attr):
+    """Return true if the user has enabled the resource."""
+    return attr in RESOURCES
+
+async def getstats():
+    """Main method."""
+    async with ClientSession(headers={'Connection': 'keep-alive'}) as session:
+        connection = vw_connection.Connection(session, VWUSER, VWPASS, country="fr")
+        if await connection.doLogin():
+            if await connection.update():
+                # Print overall state
+                # pprint(connection._state)
+
+                # Print vehicles
+                for vehicle in connection.vehicles:
+                    pprint(vehicle)
+
+                # get all instruments
+                instruments = set()
+                for vehicle in connection.vehicles:
+                    dashboard = vehicle.dashboard(mutable=True)
+
+                    for instrument in (
+                            instrument
+                            for instrument in dashboard.instruments
+                            if instrument.component in COMPONENTS
+                            and is_enabled(instrument.slug_attr)):
+
+                        instruments.add(instrument)
+
+                # Output all supported instruments
+                res = {}
+                for instrument in instruments:
+                    print(f'name: {instrument.full_name}')
+                    print(f'str_state: {instrument.str_state}')
+                    print(f'state: {instrument.state}')
+                    print(f'supported: {instrument.is_supported}')
+                    print(f'attr: {instrument.attr}')
+                    print(f'attributes: {instrument.attributes}')
+                    res[instrument.attr] = instrument.state
+                return res
+
 
 async def main():
     # Get messages from REDIS queue
@@ -49,24 +144,13 @@ async def main():
             print("--------------------------------------------------")
             results = {}
             try:
-                vehicles = vwc.search_vehicles()
-                if (vehicles and len(vehicles) > 0) and retrycpt >0:
-                    print('Trying to get stats ({} tries left)'.format(retrycpt))
-                    print('Found {} vehicles'.format(len(vehicles)))
-                    vin = vehicles[0]['vin']
-                    #results['tech'] = vwc.load_car_details(vin)
-                    results['details'] = vwc.get_vehicle_details(vin)
-                    #results['latest_report'] = vwc.get_latest_report()[0]
-                    results['latest_trip_stat'] = vwc.get_latest_trip_statistics()
-                    results['last_refuel_trip_stat'] = vwc.get_last_refuel_trip_statistics()
-                    #results['vsr'] = vwc.get_vsr()
-                    #results['psp_status'] = vwc.get_psp_status()
-                    results['location'] = vwc.get_location()
-                    results['emanager'] = vwc.get_emanager()
-                    #results['get_shutdown'] = vwc.get_shutdown()
-
-                    results['action'] = 'VWStats'
-                    results['datetime'] = callwhen
+                vwloop = asyncio.get_event_loop()
+                # loop.run(main())
+                results = []
+                results = vwloop.run_until_complete(asyncio.gather(getstats()))
+                vwloop.close()
+                results['action'] = 'VWStats'
+                results['datetime'] = callwhen
                 print(str(datetime.now()) + " Done, returning stats")
                 send_status(redis, 'vwstats', results)
             except Exception as e:
